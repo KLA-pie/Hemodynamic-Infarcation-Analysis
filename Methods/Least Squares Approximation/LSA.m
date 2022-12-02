ae1b87df32b408778a795032e7c544db6cf9859e
Kevin Lie-Atjam <klieatjam@olin.edu>
2022-12-02 02:18:54 -0500

    Defined Initial Conditions

--- Methods/Least Squares Approximation/LSA.m
+++ Methods/Least Squares Approximation/LSA.m
@@ -4,20 +4,11 @@ clc
 load('heart1.csv')
 rng(105)
 heart1randomized = heart1(randperm(size(heart1, 1)), :);
-train_data = heart1randomized(1:512,[1,4:13]);
-test_data = heart1randomized(513:1024,[1,4:13]);
+train_data = heart1randomized(1:512,[1,3:13]);
+test_data = heart1randomized(513:1024,[1,3:13]);
 
-train_boolean = heart1randomized(1:512,3);
-test_boolean = heart1randomized(513:1024,3);
-for ii = 1:512
-    if test_boolean(ii) ~= 0
-        test_boolean(ii) = 1;
-    end
-    if train_boolean(ii) ~= 0
-        train_boolean(ii) = 1;
-    end
-end
-
+train_boolean = heart1randomized(1:512,14);
+test_boolean = heart1randomized(513:1024,14);
 Weighted_Characteristics = train_data'*train_data\train_data'*train_boolean;
 Hypothesized_Train_Values = train_data*Weighted_Characteristics;
 
